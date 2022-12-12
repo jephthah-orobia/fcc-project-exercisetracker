6910d1d30c3df1d285972d7e2941e54461ea5611
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2022-12-12 15:34:50 +0800

    Converted from CommonJS to ES 6 Module

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from "node:url";
+dotenv.config();
 
-app.use(cors())
-app.use(express.static('public'))
+// create express app
+const app = express();
+
+// Middlewares, For static files and bodyParser for request.body
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(fileURLToPath(new URL('./views/index.html', import.meta.url)));
 });
 
 
-
-
-
+// start port listener
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+  console.log('Your app is listening on port ' + listener.address().port);
 })
